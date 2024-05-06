a3bde50f18765997c2dffe271ed7a2def862321d
Sophia Rosa <sophiarosaborges3@gmail.com>
2024-05-06 10:51:24 -0300

    pedindo nome do motorista

--- distancia.py
+++ distancia.py
@@ -1,7 +1,7 @@
-
+motorista = input("Digite o nome do motorista: ") #pedindo nome do motorista
 origem = input("Cidade de origem: ")
 destino = input("Cidade de destino: ")
 distancia = float(input("Qual a distância em km? "))
 velocidade = float(input("Quantos hm/h irá viajar? "))
 tempo = distancia/velocidade*60
-print(f" levará {tempo}h de {origem} à {destino}")
+print(f"{motorista} levará {tempo}h de {origem} à {destino}")
